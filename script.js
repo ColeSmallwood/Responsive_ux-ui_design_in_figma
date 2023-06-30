f38d5d8ea624358f8c8171aa8b6f3e96603dd0af
ColeSmallwood <colesmallwood@uflip.com>
2023-06-30 13:48:20 -0500

    n/a

--- script.js
+++ script.js
@@ -11,6 +11,45 @@ const homeBoxEL = document.querySelectorAll(".home-text-box");
 const homeIconEL = document.querySelectorAll(".home-icon");
 const iconEL = document.querySelectorAll(".icon");
 
+function closeDropdown() {
+  burgerEl.classList.remove("active");
+  userlogoEL.classList.remove("active");
+  ufliplogEL.classList.remove("active");
+  burgerFrameEL.classList.remove("active");
+  headerEL.classList.remove("active");
+  navDropBodyEL.classList.remove("active");
+  navContentEL.classList.remove("active");
+
+  navDropBodyEL.style.height = "0";
+  navDropBodyEL.style.opacity = "0";
+
+  homeEL.forEach(function (element) {
+    element.classList.add("hidden");
+  });
+  homeTextEL.forEach(function (element) {
+    element.classList.add("hidden");
+  });
+  homeBoxEL.forEach(function (element) {
+    element.classList.add("hidden");
+  });
+  homeIconEL.forEach(function (element) {
+    element.classList.add("hidden");
+  });
+  iconEL.forEach(function (element) {
+    element.classList.add("hidden");
+  });
+}
+
+function handleWindowResize() {
+  var windowWidth = window.innerWidth;
+
+  if (windowWidth >= 992) {
+    // Close the dropdown on larger breakpoints
+    closeDropdown();
+  }
+}
+
+// Event listener for burger click
 burgerEl.addEventListener("click", function () {
   burgerEl.classList.toggle("active");
   userlogoEL.classList.toggle("active");
@@ -62,3 +101,9 @@ burgerEl.addEventListener("click", function () {
     });
   }
 });
+
+// Event listener for window resize
+window.addEventListener("resize", handleWindowResize);
+
+// Check initial window width
+handleWindowResize();
